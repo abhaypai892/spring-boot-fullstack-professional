f7cc3f4af5f4ae43ff043127c28c9fc12283d0a1
Abhay Pai <paiabhay892@gmail.com>
2023-12-30 21:13:20 +0530

    first set of changes

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*; //newly added
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class DemoApplicationTests {
 
+	Testclass underTest = new Testclass();
 	@Test
-	void contextLoads() {
+	void addstwonumbers() {
+		//given
+		int firstnumber = 10;
+		int secondnumber = 20;
+		
+		//when
+		int result = underTest.add(firstnumber,secondnumber);
+
+		//then
+		assertThat(result).isEqualTo(30);
+	}
+
+	@Test
+	void sub2numbers() {
+
+		int f = 10;
+		int s = 20;
+		
+		int r = underTest.sub(f,s);
+		
+		assertThat(r).isEqualTo(-10);
+	}
+
+	class Testclass{
+		int add(int a, int b){
+			return a + b;
+		}
+		int sub(int a, int b){
+			return a - b;
+		}
 	}
 
 }
---
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.Test;
+
+public class StudentRepositoryTest {
+    @Test
+    void testSelectExistsEmail() {
+
+    }
+}
